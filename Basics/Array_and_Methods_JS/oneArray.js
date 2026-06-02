1a880491e70782a84369bc7f1e9783609f929136
Akash Choudhury <164205358+ezDecode@users.noreply.github.com>
2026-06-02 09:17:26 +0000

    array splice concept

---
+++ Basics/Array_and_Methods_JS/oneArray.js
@@ -0,0 +1,14 @@
+let arr = ["apple", "banana", "Litchi", "Mango"]
+
+let removed = arr.splice(0, 2, "Y", "X")
+
+console.log("Removed List items: ", removed);
+console.log("Initial Array value after removed: ", arr);
+
+
+let arr2 = ["apple", "banana", "Litchi", "Mango"]
+
+let removed2 = arr2.splice(0, 2, "Y", "X")
+
+console.log("Removed List items Two: ", removed2);
+console.log("Second Initial Array value after removed: ", arr2);
